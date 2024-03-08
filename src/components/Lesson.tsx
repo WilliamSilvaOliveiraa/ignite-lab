163c3580c8fdd5b823eceb2cc5ce1f3b8fe59bd1
William Silva <williamsilva20062005@gmail.com>
2024-03-08 16:56:38 -0300

    feat(components): *

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,16 @@
+export function Lesson() {
+  return (
+    <a href="#">
+      <span className="text-gray-300">Domingo • 20 de junho • 19h00</span>
+      <div className="rounded border border-gray-500 p-4 mt-2">
+        <header className="flex items-center justify-between">
+          <span className="text-sm text-blue-500 font-medium">
+            Conteúdo liberado
+          </span>
+          <span>AO VIVO</span>
+        </header>
+        <strong>Abertura do evento Ignite lab</strong>
+      </div>
+    </a>
+  );
+}
